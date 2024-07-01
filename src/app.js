4b7d4f02bf5f542ccdbe4a771575f7f28542dbb6
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-01 20:32:58 +0000

    project_finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generador_De_Excusas();
 };
+
+function generador_De_Excusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  let what = ["my homework", "my phone", "the car"];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let indice_who = Math.floor(Math.random() * who.length);
+  let indice_action = Math.floor(Math.random() * action.length);
+  let indice_what = Math.floor(Math.random() * what.length);
+  let indice_when = Math.floor(Math.random() * when.length);
+
+  return (
+    who[indice_who] +
+    " " +
+    action[indice_action] +
+    " " +
+    what[indice_what] +
+    " " +
+    when[indice_when]
+  );
+}
